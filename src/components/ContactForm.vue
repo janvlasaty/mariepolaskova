c17623f6496cdabf1c0609d7ebae4fabdd1f64e9
Jan Vlasatý <jan.vlasaty@melowntech.com>
2024-09-23 21:25:10 +0200

    fix: contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -12,9 +12,21 @@
       <p class="" v-html="t(`contact-form.message-sent-error`)"></p>
     </div>
   </div>
-  <form v-else @submit.prevent="submitForm">
+  <form v-else ref="form-contact-me">
     <div class="row g-3">
       <div class="col-12">
+        <input
+          v-model="fullName"
+          type="text"
+          name="full_name"
+          autocomplete="name"
+          class="form-control"
+          :placeholder="t(`contact-form.fullname`)"
+          :aria-label="t(`contact-form.fullname`)"
+          required
+        />
+      </div>
+      <!-- <div class="col-12">
         <input
           v-model="givenName"
           type="text"
@@ -23,6 +35,7 @@
           class="form-control"
           :placeholder="t(`contact-form.name`)"
           :aria-label="t(`contact-form.name`)"
+          required
         />
       </div>
       <div class="col-12">
@@ -34,8 +47,9 @@
           class="form-control"
           :placeholder="t(`contact-form.surname`)"
           :aria-label="t(`contact-form.surname`)"
+          required
         />
-      </div>
+      </div> -->
       <div class="col-12">
         <input
           v-model="email"
@@ -45,6 +59,7 @@
           class="form-control"
           :placeholder="t(`contact-form.email`)"
           :aria-label="t(`contact-form.email`)"
+          required
         />
       </div>
       <div class="col-12">
@@ -57,64 +72,77 @@
           :aria-label="t(`contact-form.message`)"
         ></textarea>
       </div>
-      <div class="col-12">
+      <!-- <div class="col-12">
         <div class="h-captcha" data-captcha="true" data-sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"></div>
-      </div>
+      </div> -->
       <div class="col-12">
         <input type="checkbox" name="botcheck" class="hidden" style="display: none" />
-        <button type="submit" class="btn btn-primary fw-bold text-white w-100">{{ t(`contact-form.submit`) }}</button>
+        <input type="hidden" name="access_key" :value="WEB3FORMS_ACCESS_KEY" />
+        <button type="submit" class="btn btn-primary fw-bold text-white w-100">
+          <span v-if="!showSending">{{ t(`contact-form.submit`) }} </span>
+          <span v-else>{{ t(`contact-form.sending`) }} </span>
+        </button>
       </div>
     </div>
   </form>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref, useTemplateRef } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 // const WEB3FORMS_ACCESS_KEY = '01f6e143-2005-48fb-b8c7-ec87f380b1d8'
+// const WEB3FORMS_ACCESS_KEY = '1c55125e-2b1b-42b0-9db8-49fc2052a0d1'
 const WEB3FORMS_ACCESS_KEY = 'cdb4edf0-dd51-4ea7-aa73-a0bfd9510fff'
-const givenName = ref('')
-const familyName = ref('')
-const email = ref('')
-const message = ref('')
+
 const showFormSentSuccess = ref(false)
 const showFormSentError = ref(false)
+const showSending = ref(false)
 
 const { t } = useI18n()
 
-const submitForm = async (e) => {
-  const hCaptcha = document.querySelector('textarea[name=h-captcha-response]').value
+const formContactMe = useTemplateRef('form-contact-me')
 
-  if (!hCaptcha) {
+onMounted(() => {
+  formContactMe.value.addEventListener('submit', async (e) => {
     e.preventDefault()
-    alert('Please fill out captcha field')
+    showSending.value = true
+    const formData = new FormData(formContactMe.value)
+    const object = Object.fromEntries(formData)
+    const json = JSON.stringify(object)
 
-    return
-  }
+    // const hCaptcha = document.querySelector('textarea[name=h-captcha-response]').value
 
-  const response = await fetch('https://api.web3forms.com/submit', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
-    },
-    body: JSON.stringify({
-      access_key: WEB3FORMS_ACCESS_KEY,
-      name: givenName.value + ' ' + familyName.value,
-      email: email.value,
-      message: message.value,
-      'h-captcha-response': window.hcaptcha.getResponse()
-    })
+    // if (!hCaptcha) {
+    //   e.preventDefault()
+    //   alert('Please fill out captcha field')
+
+    //   return
+    // }
+
+    try {
+      const response = await fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: json,
+        timeout: 5000
+      })
+      const result = await response.json()
+
+      if (result.success) {
+        showFormSentSuccess.value = true
+      } else {
+        throw new Error('Error')
+      }
+    } catch (error) {
+      showFormSentError.value = true
+    }
+    showSending.value = false
   })
-  const result = await response.json()
-
-  if (result.success) {
-    showFormSentSuccess.value = true
-  } else {
-    showFormSentError.value = true
-  }
-}
+})
 </script>
 
 <style lang="scss" scoped>
